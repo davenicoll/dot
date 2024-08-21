1027f026a82262f0a18e98896274dd035caba14c
Dave Nicoll <690117+davenicoll@users.noreply.github.com>
2024-08-20 17:25:09 -0700

    many macos settings

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ ln -sf "$PWD/com.googlecode.iterm2.plist" "$HOME/Library/Preferences/com.googlec
 rm ~/Library/Fonts/JetBrainsMonoNerd*
 rm ~/Library/Fonts/JetBrainsMonoNLNerdFont*
 
-# Install warp theme
-mkdir -p "$HOME/.warp/themes"
-ln -sf "$PWD/.dn-warp-theme.yaml" "$HOME/.warp/themes/dn-warp-theme.yaml"
-
 # Symlink font files
 for font_file in fonts/*.ttf; do
   base_name=$(basename "$font_file")
@@ -23,6 +19,7 @@ done
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+export PATH="$HOME/homebrew/bin:$HOME/homebrew/sbin:$PATH"
 
 # Install formulas
 brew install shellcheck eza fzf zoxide
@@ -32,15 +29,23 @@ brew install --cask mac-mouse-fix rectangle shottr raycast iterm2 copyclip monit
 
 # Mac specific settings
 
+# Authenticate as admin up front
+sudo -v
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # Disable Mission Control spaces (drag to screen top)
 defaults write com.apple.dock mcx-expose-disabled -bool TRUE && killall Dock
 
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
 # Always show scrollbars
 defaults write "Apple Global Domain" AppleShowScrollBars -string Always
-
-# Set keyboard typing preferences
-defaults write -g InitialKeyRepeat -int 35 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 2         # normal minimum is 2 (30 ms)
 
 # Enable/disable menu bar items and set their position
 defaults write "com.apple.controlcenter" "NSStatusItem Preferred Position Battery" 260
@@ -70,34 +75,9 @@ defaults write "com.apple.WindowManager" EnableStandardClickToShowDesktop 0
 defaults write com.apple.assistant.support "Assistant Enabled" -bool false
 defaults write com.apple.Siri StatusMenuVisible -bool false
 
-# Configure dock
-defaults write "com.apple.dock" "minimize-to-application" -bool true
-defaults write "com.apple.dock" "show-recents" -bool false
-defaults write com.apple.dock tilesize -int 39
-
-# Configure Finder
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-# Finder: show status bar
-defaults write com.apple.finder ShowStatusBar -bool true
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
-# When performing a search, search the current folder by default
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-# Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-# Show the ~/Library folder
-chflags nohidden ~/Library
-# Show the /Volumes folder
-sudo chflags nohidden /Volumes
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
 
 # Enable home and end keys on external keyboard
 mkdir -p ~/Library/KeyBindings
@@ -113,6 +93,117 @@ cat <<EOF >~/Library/KeyBindings/DefaultKeyBinding.dict
   "^$\UF72B" = moveToEndOfDocumentAndModifySelection:; // ctrl-shift-end
 }
 EOF
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a faster keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 30
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: map bottom right corner to right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Increase sound quality for Bluetooth headphones/headsets
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Stop iTunes from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# Avoid creating .DS_Store files on USB drives
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Enable snap-to-grid for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+  General -bool true \
+  OpenWith -bool true \
+  Privileges -bool true
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+# Configure dock
+defaults write "com.apple.dock" "minimize-to-application" -bool true
+defaults write "com.apple.dock" "show-recents" -bool false
+defaults write com.apple.dock tilesize -int 39
 
 # Add Home and Downloads folders to the dock
 
@@ -233,5 +324,90 @@ defaults write "com.apple.dock" "persistent-others" \
 
 killall Dock # restart the dock
 
-# Accept the xcode license
-sudo xcodebuild -license accept
+###############################################################################
+# Spotlight                                                                   #
+###############################################################################
+
+# Hide Spotlight tray-icon (and subsequent helper)
+#sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+# Disable Spotlight indexing for any volume that gets mounted and has not yet
+# been indexed before.
+# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+# Change indexing order and disable some search results
+# Yosemite-specific search results (remove them if you are using macOS 10.9 or older):
+# 	MENU_DEFINITION
+# 	MENU_CONVERSION
+# 	MENU_EXPRESSION
+# 	MENU_SPOTLIGHT_SUGGESTIONS (send search queries to Apple)
+# 	MENU_WEBSEARCH             (send search queries to Apple)
+# 	MENU_OTHER
+defaults write com.apple.spotlight orderedItems -array \
+  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}'
+# Load new settings before rebuilding the index
+killall mds >/dev/null 2>&1
+# Make sure indexing is enabled for the main volume
+sudo mdutil -i on / >/dev/null
+# Rebuild the index from scratch
+sudo mdutil -E / >/dev/null
+
+###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &>/dev/null && sudo tmutil disablelocal
+
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+
+###############################################################################
+# XCode                                                                       #
+###############################################################################
+
+print_result() {
+  [ $1 -eq 0 ] &&
+    print_success "$2" ||
+    print_error "$2"
+
+  return $1
+}
+
+if ! xcode-select --print-path &>/dev/null; then
+
+  # Prompt user to install the XCode Command Line Tools
+  xcode-select --install &>/dev/null
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Wait until the XCode Command Line Tools are installed
+  until xcode-select --print-path &>/dev/null; do
+    sleep 5
+  done
+
+  print_result $? 'Install XCode Command Line Tools'
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Point the `xcode-select` developer directory to
+  # the appropriate directory from within `Xcode.app`
+  # https://github.com/alrra/dotfiles/issues/13
+
+  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+  print_result $? 'Make "xcode-select" developer directory point to Xcode'
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # Prompt user to agree to the terms of the Xcode license
+  # https://github.com/alrra/dotfiles/issues/10
+
+  sudo xcodebuild -license
+  print_result $? 'Agree with the XCode Command Line Tools licence'
+
+fi
+
+print_result $? 'XCode Command Line Tools'
