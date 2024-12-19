e13c05ab4d22e994fa585e60fdef983beb6be778
Dave Nicoll <690117+davenicoll@users.noreply.github.com>
2024-12-19 15:52:11 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -421,7 +421,7 @@ if ! xcode-select --print-path &>/dev/null; then
   # Prompt user to agree to the terms of the Xcode license
   # https://github.com/alrra/dotfiles/issues/10
 
-  sudo xcodebuild -license
+  sudo xcodebuild -license accept
   print_result $? 'Agree with the XCode Command Line Tools licence'
 
 fi
